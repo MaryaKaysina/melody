828d9fb8c5e203c522e785b9186130187f1081b9
MaryaKaysina <linkinpark335@gmail.com>
2021-08-20 14:23:43 +0500

    create navigation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   let currentFloor = 2;
   let floorPath = $('.main__home path');
+  let flatPath = $('.modal__floors path');
   let counterUp = $('.main__btn--up');
   let counterDown = $('.main__btn--down');
   let modal = $('.modal');
@@ -13,10 +14,18 @@ $(document).ready(function () {
     modalCounter.text(usCurrentFloor);
     modal.toggleClass('is-open');
   }
+
   floorPath.on('mouseover', function() {
     floorPath.removeClass('current-floor');
     currentFloor = $(this).attr('data-floor');
     $('.main__counter').text(currentFloor);
+  });
+
+  flatPath.on('mouseover', function() {
+    $('.modal__link').removeClass('current-flat');
+    currentFlat = $(this).attr('data-flat');
+    console.log(currentFlat);
+    $(`#${currentFlat}`).toggleClass('current-flat');
   });
 
   floorPath.on('click', toggleModal);
